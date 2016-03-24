ad9f3de0ae13c5351bd0ce6d82f31ad4162b00d1
pavel8silber <paul8julia@gmail.com>
2016-03-24 14:43:17 +0200

    Small changes

--- my_test_words_counter.py
+++ my_test_words_counter.py
@@ -6,15 +6,17 @@ def print_words():
     import string
     f = open('text.txt', 'r')       # чтение из текст файла
     f.seek(0)
-    str = f.read().decode('utf-8').split()
+    str = f.read().decode('utf-8').split()     # decode string
 
-    my_d = dict()
-    my_set =set()
+    print str[23]
+
+    my_d = dict()                           # new dict
+    my_set = set()                          # new set
 
     for i in str:                   # пропускаю через множества
         my_set.add(i)
 
-    for word in my_set:
+    for word in str:
         new_word = word.lower()
         new_word1 = new_word.strip(string.punctuation)  # удаление знаков припенания
 
@@ -25,8 +27,9 @@ def print_words():
                 my_d[ky] += 1
 
     for y in my_d:
-        print '{0} - {1}'.format(y.encode('utf-8'), my_d[y])
+        print '{1} - {0}'.format(y.encode('utf-8'), my_d[y])
         #print '%s - %d' % (y, my_d[y])    # Вывод на экран
+        # print 'test'
     f.close()
     return
 
